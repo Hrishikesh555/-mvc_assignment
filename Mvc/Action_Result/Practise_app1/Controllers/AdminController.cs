06dcca2ffee77695117456702e4c79b7eb9baca1
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2022-01-20 15:47:42 +0530

    First Commit

---
+++ Mvc/Action_Result/Practise_app1/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practise_app1.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
