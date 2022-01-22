e4455d15a210c6eccd64786ca26f9075399c70fd
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2022-01-23 00:43:55 +0530

    First Commit

---
+++ Mvc/SharedView/Practise_app4/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practise_app4.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult accountt()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Tollfree = "22-22-23-33";
+            return View();
+        }
+    }
+}
---
+++ Mvc/SharedView/Practise_app4/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practise_app4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Tollfree = "11-22-23-33";
+            return View();
+        }
+    }
+}
