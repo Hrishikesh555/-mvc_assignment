f960b4b15fa05ced9be9b3b86ab48bba54e9a93e
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2022-01-23 16:42:41 +0530

    First Commit

---
+++ Mvc/Layout1/Practise_app5/Controllers/HomController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practise_app5.Controllers
+{
+    public class HomController : Controller
+    {
+        // GET: Hom
+        public ActionResult index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Profilee()
+        {
+            return View();
+        }
+    }
+}
