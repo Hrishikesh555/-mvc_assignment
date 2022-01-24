74ab90bfb80394075a8251e6ad4a61d9f4f9f9b8
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2022-01-24 23:32:12 +0530

    First Commit

---
+++ Mvc/Model_binding/Practise_app7/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practise_app7.Models;
+
+namespace Practise_app7.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product(){ProductId=101,ProductName="Phone",Rate=8500 },
+                new Product(){ProductId=102,ProductName="LAptop",Rate=85000 },
+                new Product(){ProductId=103,ProductName="Machine",Rate=55000 }
+            };
+           
+            return View(products);
+        }
+        public ActionResult Details(int Id)
+
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product(){ProductId=101,ProductName="Phone",Rate=8500 },
+                new Product(){ProductId=102,ProductName="LAptop",Rate=85000 },
+                new Product(){ProductId=103,ProductName="Machine",Rate=55000 }
+            };
+            Product matchingprod=null;
+            foreach (var item in products)
+            {
+                if(item.ProductId==Id)
+                {
+                    matchingprod = item;
+                }
+            }
+    
+            return View(matchingprod);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mvc/Model_binding/Practise_app7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practise_app7.MvcApplication" Language="C#" %>
