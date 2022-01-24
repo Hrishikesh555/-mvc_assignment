b48db18d4b4508122817b0c31fd013acad5fe72c
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2022-01-24 12:01:38 +0530

    First Commit

---
+++ Mvc/RoutingId_Constraints/Practise_app5/App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Practise_app5
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            /*routes.MapRoute(
+              name: "Product",
+              url: "Product/Products/{productNm}",
+               defaults: new { controller = "Product", action = "Products", id = UrlParameter.Optional }
+          );*/
+            routes.MapRoute(
+              name: "Product",
+              url: "{controller}/{action}/{productNm}",
+               defaults: new { },
+                constraints: new { productNm = @"^[A-Za-z ]*$" }
+              // constraints:new {productNm=@"[A-Za-z]^*$"}
+          );
+
+            routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Hom", action = "index", id = UrlParameter.Optional }
+           );
+           
+
+        }
+    }
+}
---
+++ Mvc/RoutingId_Constraints/Practise_app5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practise_app5.MvcApplication" Language="C#" %>
