04e0e76b5c7adab51610396608ecba0a3ab6abf9
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2022-01-25 10:49:49 +0530

    First Commit

---
+++ Mvc/Custom_ModelBinder/Practise_app7/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practise_app7.Models;
+
+namespace Practise_app7.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([ModelBinder(typeof(CustomeBinder))] Student stud)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mvc/Custom_ModelBinder/Practise_app7/Models/CustomeBinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practise_app7.Models
+{
+    public class CustomeBinder:IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext,ModelBindingContext bindingContext)
+        {
+            int StudentId =Convert.ToInt32(controllerContext.HttpContext.Request.Form["StudentId"]);
+            string StudentName = controllerContext.HttpContext.Request.Form["StudentName"];
+            string Dno = controllerContext.HttpContext.Request.Form["Dno"];
+            string Landmark = controllerContext.HttpContext.Request.Form["Landmark"];
+            string City = controllerContext.HttpContext.Request.Form["City"];
+
+            return new Student() { StudentId = StudentId, StudentName = StudentName, Adress = Dno + " ," + Landmark + "," + City };
+        }
+    }
+}
