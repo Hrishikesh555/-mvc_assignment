28533f325bc28d7fbb8a04d789ce8979775e145a
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2022-01-27 15:22:54 +0530

    First Commit

---
+++ Mvc/CodeFirstAutoMigration/EntityDBFIRST/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityDBFIRST.Models;
+
+namespace EntityDBFIRST.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            CompanyDbCotext db = new CompanyDbCotext();
+            List<Category> categories =db.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
---
+++ Mvc/CodeFirstAutoMigration/EntityDBFIRST/Models/CompanyDbCotext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using EntityDBFIRST.Migrations;
+
+namespace EntityDBFIRST.Models
+{
+    public class CompanyDbCotext:DbContext
+        
+    {
+        public CompanyDbCotext():base("MyConnectionString")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<CompanyDbCotext,Configuration>());
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
