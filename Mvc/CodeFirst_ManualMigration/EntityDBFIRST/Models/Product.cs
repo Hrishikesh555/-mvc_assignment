94a07533cfee7fc500aa01ba038cf7f12c1bb540
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2022-01-27 17:51:43 +0530

    First Commit

---
+++ Mvc/CodeFirst_ManualMigration/EntityDBFIRST/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityDBFIRST.Models
+{
+    public class Product
+    {
+        [Key]
+        public long ProductID { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<System.DateTime> DateofPurchase { get; set; }
+        public string AvailabilityStatus { get; set; }
+        public Nullable<long> CategoryID { get; set; }
+        public Nullable<long> BrandID { get; set; }
+        public Nullable<bool> Active { get; set; }
+        public string Photo { get; set; }
+
+       public Nullable<decimal>  Quantity { get; set; }
+        public virtual Brand Brand { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
