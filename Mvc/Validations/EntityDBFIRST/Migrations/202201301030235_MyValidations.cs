90a15d7b14602b2780a85c02399852d1c3355b21
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2022-01-30 17:39:42 +0530

    First Commit

---
+++ Mvc/Validations/EntityDBFIRST/Migrations/202201301030235_MyValidations.cs
@@ -0,0 +1,16 @@
+namespace EntityDBFIRST.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MyValidations : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Mvc/Validations/EntityDBFIRST/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityDBFIRST.Models
+{
+    public class Product
+    {
+        [Key]
+        [Display (Name = "Product Id")]
+        public long ProductID { get; set; }
+
+        [Required (ErrorMessage ="Product name cannot be kept blank")]
+        [Display(Name="Product Name")]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage ="Alphabet only")]
+        [MinLength(2,ErrorMessage ="name should Be greater than 2 alphabet")]
+        public string ProductName { get; set; }
+  
+        [Required]
+        [Range(0,100000,ErrorMessage ="Price should be between 0to 100000")]
+        public Nullable<decimal> Price { get; set; }
+
+        [Display(Name = "Date of Purchase")]
+        public Nullable<System.DateTime> DateofPurchase { get; set; }
+        public string AvailabilityStatus { get; set; }
+
+        [Required]
+        public Nullable<long> CategoryID { get; set; }
+        [Required]
+        public Nullable<long> BrandID { get; set; }
+        public Nullable<bool> Active { get; set; }
+        public string Photo { get; set; }
+
+       public Nullable<decimal>  Quantity { get; set; }
+        public virtual Brand Brand { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
